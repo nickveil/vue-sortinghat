8afdc69a824f0046caec0d9cd419889aaa93477c
nickveil <31191433+nickveil@users.noreply.github.com>
2017-10-13 15:12:07 -0400

    Figured out loging and some styling

--- app/main.js
+++ app/main.js
@@ -1,3 +1,8 @@
+Vue.component('selections', {
+	template: '<li><button v-on:click="displayQuestions(house)">{{text}}</button></li>',
+	props: ['text','house','displayQuestions']
+})
+
 var sortingHat = new Vue( {
 
 	el:'#sortingHat',
@@ -8,142 +13,145 @@ var sortingHat = new Vue( {
   	selectedHouse : '',
   	selectedQuestions : '',
   	whichType:0,
+  	listOfAnswers:[],
+
 
 		'Houses': [
 
-    {
-      name: 'Gryffindor',
-      description: ''
-    },
-    {
-      name: 'Hufflepuff',
-      description: ''
-    },
-    {
-      name: 'Ravenclaw',
-      description: ''
-    },
-    {
-      name: 'Slytherin',
-      description: ''
-    }
+	    {
+	      name: 'Gryffindor',
+	      description: ''
+	    },
+	    {
+	      name: 'Hufflepuff',
+	      description: ''
+	    },
+	    {
+	      name: 'Ravenclaw',
+	      description: ''
+	    },
+	    {
+	      name: 'Slytherin',
+	      description: ''
+	    }
 
-  ],
+	  ],
 
-  'Questions': [
+	  'Questions': [
 
-    { 
-      text: 'What is your favorite color?',
-      answers: [
-        {
-          text: 'Scarlet',
-          house: 'Gryffindor'
-        },
-        {
-          text: 'Yellow',
-          house: 'Hufflepuff'
-        },
-        {
-          text: 'Blue',
-          house: 'Ravenclaw'
-        },
-        {
-          text: 'Green',
-          house: 'Slytherin'
-        }
-      ]
-    },
-    { 
-      text: 'What is your favorite animal?',
-      answers: [
-        {
-          text: 'Lion',
-          house: 'Gryffindor'
-        },
-        {
-          text: 'Badger',
-          house: 'Hufflepuff'
-        },
-        {
-          text: 'Eagle',
-          house: 'Ravenclaw'
-        },
-        {
-          text: 'Snake',
-          house: 'Slytherin'
-        }
-      ]
-    },
-    { 
-      text: 'What kind of person do you try to be?',
-      answers: [
-        {
-          text: 'Brave',
-          house: 'Gryffindor'
-        },
-        {
-          text: 'Loyal',
-          house: 'Hufflepuff'
-        },
-        {
-          text: 'Witty',
-          house: 'Ravenclaw'
-        },
-        {
-          text: 'Cunning',
-          house: 'Slytherin'
-        }
-      ]
-    },
-    { 
-      text: 'Who is your favorite witch or wizard?',
-      answers: [
-        {
-          text: 'Albus Dumbledore',
-          house: 'Gryffindor'
-        },
-        {
-          text: 'Newt Scamander',
-          house: 'Hufflepuff'
-        },
-        {
-          text: 'Garrick Ollivander',
-          house: 'Ravenclaw'
-        },
-        {
-          text: 'Tom Riddle',
-          house: 'Slytherin'
-        }
-      ]
-    },
-    { 
-      text: 'What is your favorite Berty Bott\'s flavor?',
-      answers: [
-        {
-          text: 'Grape',
-          house: 'Gryffindor'
-        },
-        {
-          text: 'Horseradish',
-          house: 'Hufflepuff'
-        },
-        {
-          text: 'Roast Beef',
-          house: 'Ravenclaw'
-        },
-        {
-          text: 'Spinach',
-          house: 'Slytherin'
-        }
-      ]
-    }
+	    { 
+	      text: 'What is your favorite color?',
+	      answers: [
+	        {
+	          text: 'Scarlet',
+	          house: 'Gryffindor'
+	        },
+	        {
+	          text: 'Yellow',
+	          house: 'Hufflepuff'
+	        },
+	        {
+	          text: 'Blue',
+	          house: 'Ravenclaw'
+	        },
+	        {
+	          text: 'Green',
+	          house: 'Slytherin'
+	        }
+	      ]
+	    },
+	    { 
+	      text: 'What is your favorite animal?',
+	      answers: [
+	        {
+	          text: 'Lion',
+	          house: 'Gryffindor'
+	        },
+	        {
+	          text: 'Badger',
+	          house: 'Hufflepuff'
+	        },
+	        {
+	          text: 'Eagle',
+	          house: 'Ravenclaw'
+	        },
+	        {
+	          text: 'Snake',
+	          house: 'Slytherin'
+	        }
+	      ]
+	    },
+	    { 
+	      text: 'What kind of person do you try to be?',
+	      answers: [
+	        {
+	          text: 'Brave',
+	          house: 'Gryffindor'
+	        },
+	        {
+	          text: 'Loyal',
+	          house: 'Hufflepuff'
+	        },
+	        {
+	          text: 'Witty',
+	          house: 'Ravenclaw'
+	        },
+	        {
+	          text: 'Cunning',
+	          house: 'Slytherin'
+	        }
+	      ]
+	    },
+	    { 
+	      text: 'Who is your favorite witch or wizard?',
+	      answers: [
+	        {
+	          text: 'Albus Dumbledore',
+	          house: 'Gryffindor'
+	        },
+	        {
+	          text: 'Newt Scamander',
+	          house: 'Hufflepuff'
+	        },
+	        {
+	          text: 'Garrick Ollivander',
+	          house: 'Ravenclaw'
+	        },
+	        {
+	          text: 'Tom Riddle',
+	          house: 'Slytherin'
+	        }
+	      ]
+	    },
+	    { 
+	      text: 'What is your favorite Berty Bott\'s flavor?',
+	      answers: [
+	        {
+	          text: 'Grape',
+	          house: 'Gryffindor'
+	        },
+	        {
+	          text: 'Horseradish',
+	          house: 'Hufflepuff'
+	        },
+	        {
+	          text: 'Roast Beef',
+	          house: 'Ravenclaw'
+	        },
+	        {
+	          text: 'Spinach',
+	          house: 'Slytherin'
+	        }
+	      ]
+	    }
 
-  ]
+	  ]
 
 	
   },
 
 	methods : {
+
 
 		houseOrQuestion: function(){
 
@@ -161,12 +169,32 @@ var sortingHat = new Vue( {
 		randomHouse : function(){
 			const idx = Math.floor(Math.random() * this.Houses.length);
 	    this.selectedHouse = this.Houses[idx].name;
-			
+			console.log("this.selectedHouse: ",this.selectedHouse);
 		},
 
 		randomQuestion : function(){
-			const idx = Math.floor(Math.random() * this.Questions.length);
+			const idx = Math.floor(Math.random() * 5 /*this.Questions.length*/);
 	    this.selectedQuestions = this.Questions[idx].text;
+	    this.listOfAnswers=this.Questions[idx].answers;
+	    
+
+	    
+	    
+		},
+
+		displayQuestions : function(house){
+
+			
+			this.selectedHouse = house;
+
+		},
+
+		onClick:function(event){
+			if (event.target.className == 'disabled'){
+				return;
+
+			}
+			event.target.className = 'disabled';
 		}
 
 	}
