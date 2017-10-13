bd85c5b951a2408f6ce053d3c0951fbef3e613ec
nickveil <31191433+nickveil@users.noreply.github.com>
2017-10-13 12:12:40 -0400

    random question when random house not selected works

--- app/main.js
+++ app/main.js
@@ -6,6 +6,8 @@ var sortingHat = new Vue( {
 
 		newName :'',
   	selectedHouse : '',
+  	selectedQuestions : '',
+  	whichType:0,
 
 		'Houses': [
 
@@ -28,15 +30,143 @@ var sortingHat = new Vue( {
 
   ],
 
+  'Questions': [
+
+    { 
+      text: 'What is your favorite color?',
+      answers: [
+        {
+          text: 'Scarlet',
+          house: 'Gryffindor'
+        },
+        {
+          text: 'Yellow',
+          house: 'Hufflepuff'
+        },
+        {
+          text: 'Blue',
+          house: 'Ravenclaw'
+        },
+        {
+          text: 'Green',
+          house: 'Slytherin'
+        }
+      ]
+    },
+    { 
+      text: 'What is your favorite animal?',
+      answers: [
+        {
+          text: 'Lion',
+          house: 'Gryffindor'
+        },
+        {
+          text: 'Badger',
+          house: 'Hufflepuff'
+        },
+        {
+          text: 'Eagle',
+          house: 'Ravenclaw'
+        },
+        {
+          text: 'Snake',
+          house: 'Slytherin'
+        }
+      ]
+    },
+    { 
+      text: 'What kind of person do you try to be?',
+      answers: [
+        {
+          text: 'Brave',
+          house: 'Gryffindor'
+        },
+        {
+          text: 'Loyal',
+          house: 'Hufflepuff'
+        },
+        {
+          text: 'Witty',
+          house: 'Ravenclaw'
+        },
+        {
+          text: 'Cunning',
+          house: 'Slytherin'
+        }
+      ]
+    },
+    { 
+      text: 'Who is your favorite witch or wizard?',
+      answers: [
+        {
+          text: 'Albus Dumbledore',
+          house: 'Gryffindor'
+        },
+        {
+          text: 'Newt Scamander',
+          house: 'Hufflepuff'
+        },
+        {
+          text: 'Garrick Ollivander',
+          house: 'Ravenclaw'
+        },
+        {
+          text: 'Tom Riddle',
+          house: 'Slytherin'
+        }
+      ]
+    },
+    { 
+      text: 'What is your favorite Berty Bott\'s flavor?',
+      answers: [
+        {
+          text: 'Grape',
+          house: 'Gryffindor'
+        },
+        {
+          text: 'Horseradish',
+          house: 'Hufflepuff'
+        },
+        {
+          text: 'Roast Beef',
+          house: 'Ravenclaw'
+        },
+        {
+          text: 'Spinach',
+          house: 'Slytherin'
+        }
+      ]
+    }
+
+  ]
+
 	
   },
 
 	methods : {
 
-		addQuestion : function(){
+		houseOrQuestion: function(){
+
+			const idx = Math.floor(Math.random() * 10);
+			var whichType=idx;
+
+			if(whichType>4){
+				this.randomHouse();
+			}else{
+				this.randomQuestion();
+			}
+
+		},
+
+		randomHouse : function(){
 			const idx = Math.floor(Math.random() * this.Houses.length);
 	    this.selectedHouse = this.Houses[idx].name;
 			
+		},
+
+		randomQuestion : function(){
+			const idx = Math.floor(Math.random() * this.Questions.length);
+	    this.selectedQuestions = this.Questions[idx].text;
 		}
 
 	}
